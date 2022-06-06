a39ceb45f83e5d646ba3d2ccae2a19871b044a79
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-06-06 01:47:08 -0400

    commit before pushing to heroku

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { application } = require('express');
 const PORT = 5500;
 
 app.use(cors());
 app.use(express.static('public'));
 
 const products = {
-  'buffalo style seasoned almonds': {
+  buffalo: {
+    name: 'Buffalo Style Seasoned Almonds',
     price: '3.99',
     category: 'Nuts, Dried Fruits, Seeds',
     sku: '73689',
@@ -16,6 +18,7 @@ const products = {
     ingredients: 'almonds, vinegar, red pepper, salt, garlic, paprika',
   },
   brookie: {
+    name: 'Brookie',
     price: '4.49',
     category: 'Sweet Stuff',
     sku: '61281',
@@ -24,6 +27,7 @@ const products = {
     ingredients: 'cookie batter, brownie batter, love',
   },
   caius: {
+    name: 'Caius Rigsby',
     price: '666.66',
     category: 'Sweet Stuff',
     sku: '42069',
@@ -32,6 +36,7 @@ const products = {
     ingredients: 'just a bunch of sh*t that was kind of there',
   },
   unknown: {
+    name: 'Unknown',
     price: 'unknown',
     category: 'unknown',
     sku: 'unknown',
@@ -45,11 +50,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/api', (req, res) => {
+  res.json(products);
+});
+
 app.get('/api/:product', (req, res) => {
   const productName = req.params.product.toLowerCase();
 
   if (products[productName]) {
-    console.log(res.json(products[productName]));
+    // console.log(res.json(products[productName]));
     res.json(products[productName]);
   }
   res.json(products['unknown']);
